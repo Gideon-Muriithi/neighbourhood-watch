61d67aee05139768343f92c786e37ff701c6217e
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-25 15:29:45 +0300

    Create profile functionality

--- watch/models.py
+++ watch/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Neighbourhood(models.Model):
+    neighbourhood = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.neighbourhood
+
+    def save_neighbourhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls,neighbourhood):
+        cls.objects.filter(neighbourhood=neighbourhood).delete()
+
+
+
+class Profile(models.Model):
+    profile_image = models.ImageField(upload_to='profile_pics/')
+    neighbourhood_name = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    neighbourhood_description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
