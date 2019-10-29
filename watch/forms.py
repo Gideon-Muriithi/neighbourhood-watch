af519bb0b6ec72fedcb5e8966a0460bb388cfc9e
Gideon-Muriithi <gideongakenge@gmail.com>
2019-10-29 15:57:54 +0300

    Create forms

--- watch/forms.py
+++ watch/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
-from .models import Profile, Neighbourhood
+from .models import Profile, Neighbourhood, Business, Post
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -20,3 +20,12 @@ class AddHoodForm(forms.ModelForm):
         model = Neighbourhood
         exclude = ['user_profile', 'profile']
 
+class AddBusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['business_owner', 'business_hood']
+
+class AddPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['poster', 'post_hood']
